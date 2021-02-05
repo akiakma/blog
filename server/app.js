f67b874b8b7655eb69ae012dddbdacdc4a9478b3
Hyunjun Kim <hyunjunkim@Hyunjunui-MacBookPro.local>
2021-02-05 23:52:20 +0900

    완성본

--- server/app.js
+++ server/app.js
@@ -44,7 +44,7 @@ app.use("/api/search", searchRoutes);
 if (prod) {
     app.use(express.static(path.join(__dirname, "../client/build")));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../client/build", index.html));
+        res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
     });
 }
 
